b160ba7850038c7668d51f9d6f3fb657e1148476
TemirlanNurligenov <66307255+TemirlanNurligenov@users.noreply.github.com>
2021-11-01 03:13:18 +0600

    Updating settings

--- product/views.py
+++ product/views.py
@@ -101,7 +101,7 @@ def index(request):
                 f.write(imgdata)
             name, confidence = validate_face(filename)
             print(name, confidence)
-            if confidence:
+            if confidence >= 50:
                 return JsonResponse({'data': 'Success'})
         return JsonResponse({'data': 'Error'})
     return render(request, 'index.html')
